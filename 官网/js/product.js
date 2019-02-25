1e4aa8a880f4591a22f63115558f43fc9a553cb4
lily <1766686344@qq.com>
2019-02-25 17:06:58 +0800

    1

--- 官网/js/product.js
+++ 官网/js/product.js
@@ -2,13 +2,18 @@
     $(".imgW").hover(function(){
         $(this).find(".mengban").show();
         $(this).find(".inner_p").show();  
-        $(this).find(".outW").removeClass("smaller").addClass("bigger");
+        //图片放大
+        $(this).find(".imgBox").css("transform","scale(1.1)")
         $(this).find(".p1").removeClass("fadeOutUp").addClass("fadeInDown");
         $(this).find(".p2").removeClass("fadeOutDown").addClass("fadeInUp");
+        $(this).find(".p18 p").removeClass("fadeInUp").addClass("fadeOutDown");
     },function(){
-        $(this).find(".outW").removeClass("bigger").addClass("smaller");
+        //图片还原
+        $(this).find(".imgBox").css("transform","scale(1)");
+       
         $(this).find(".mengban").hide();
         $(this).find(".p1").removeClass("fadeInDown").addClass("fadeOutUp");
         $(this).find(".p2").removeClass("fadeInUp").addClass("fadeOutDown");
+        $(this).find(".p18 p").removeClass("fadeOutDown").addClass("fadeInUp");
     })
 })
